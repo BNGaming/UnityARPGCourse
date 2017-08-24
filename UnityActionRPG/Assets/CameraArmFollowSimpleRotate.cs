74e22a58dbb383c1260b0d6f14bb68c64755bf6b
Charles Unruh <cunruh3760@gmail.com>
2017-08-23 21:02:22 -0400

    Camera Rotation

---
+++ UnityActionRPG/Assets/CameraArmFollowSimpleRotate.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraArmFollowSimpleRotate : MonoBehaviour {
+
+    GameObject player;
+    public float rotationAngle;
+
+    // Use this for initialization
+    void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player");
+    }
+
+    // Update is called once per frame
+    void LateUpdate()
+    {
+        transform.position = player.transform.position;
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            transform.Rotate(Vector3.up, rotationAngle);
+        }
+        else if (Input.GetKeyDown(KeyCode.Q))
+        {
+            transform.Rotate(Vector3.up, -rotationAngle);
+        }
+    }
+}
